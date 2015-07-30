8cda7d342e74eebdb12cda62b69de1bda492b6ad
Ajay Garg <ajay.garg@sensegrow.com>
2015-07-30 13:22:14 +0530

    Not using standard-include-directories as default, instead specifying them manually.
    This is required to cater to different architectures/devices.

--- client/build_ubuntu_14_04.sh
+++ client/build_ubuntu_14_04.sh
@@ -1,9 +1,43 @@
+SYSTEM_INCLUDES=`echo                                               \
+        -I/usr/lib/gcc/i686-linux-gnu/4.8/include                   \
+        -I/usr/local/include                                        \
+        -I/usr/lib/gcc/i686-linux-gnu/4.8/include-fixed             \
+        -I/usr/include/i386-linux-gnu                               \
+        -I/usr/include `
+
 THREAD_IMPL="pthread"
 TIME_IMPL="sys_timeval"
 NETWORK_COMMUNICATION_IMPL="tcp_socket"
 FS_COMMUNICATION_IMPL="linux_fs"
 
-
 EXTRA_LINK_LIBS="-lpthread"
 
-gcc stdoutsub.c -I ../MQTTPacket/src src/common/instamsg.c src/common/log.c src/common/config.c src/threading/${THREAD_IMPL}.c src/time/${TIME_IMPL}.c src/communication/network/${NETWORK_COMMUNICATION_IMPL}.c src/communication/fs/${FS_COMMUNICATION_IMPL}.c ../MQTTPacket/src/MQTTFormat.c  ../MQTTPacket/src/MQTTPacket.c ../MQTTPacket/src/MQTTDeserializePublish.c ../MQTTPacket/src/MQTTConnectClient.c ../MQTTPacket/src/MQTTSubscribeClient.c ../MQTTPacket/src/MQTTSerializePublish.c -o stdoutsub ../MQTTPacket/src/MQTTConnectServer.c ../MQTTPacket/src/MQTTSubscribeServer.c ../MQTTPacket/src/MQTTUnsubscribeServer.c ../MQTTPacket/src/MQTTUnsubscribeClient.c ${EXTRA_LINK_LIBS}
+gcc -nostdinc                                                       \
+        stdoutsub.c                                                 \
+                                                                    \
+        ${SYSTEM_INCLUDES}                                          \
+        -I ../MQTTPacket/src                                        \
+                                                                    \
+        src/common/instamsg.c                                       \
+        src/common/log.c                                            \
+        src/common/config.c                                         \
+                                                                    \
+        ../MQTTPacket/src/MQTTFormat.c                              \
+        ../MQTTPacket/src/MQTTPacket.c                              \
+        ../MQTTPacket/src/MQTTDeserializePublish.c                  \
+        ../MQTTPacket/src/MQTTConnectClient.c                       \
+        ../MQTTPacket/src/MQTTSubscribeClient.c                     \
+        ../MQTTPacket/src/MQTTSerializePublish.c                    \
+        ../MQTTPacket/src/MQTTConnectServer.c                       \
+        ../MQTTPacket/src/MQTTSubscribeServer.c                     \
+        ../MQTTPacket/src/MQTTUnsubscribeServer.c                   \
+        ../MQTTPacket/src/MQTTUnsubscribeClient.c                   \
+                                                                    \
+        src/threading/${THREAD_IMPL}.c                              \
+        src/time/${TIME_IMPL}.c                                     \
+        src/communication/network/${NETWORK_COMMUNICATION_IMPL}.c   \
+        src/communication/fs/${FS_COMMUNICATION_IMPL}.c             \
+                                                                    \
+        -o stdoutsub                                                \
+                                                                    \
+        ${EXTRA_LINK_LIBS}
