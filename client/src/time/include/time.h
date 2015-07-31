c21c8ef38cd04c41c39c1c8969b1b10b72ee20cb
Ajay Garg <ajay.garg@sensegrow.com>
2015-07-31 13:13:19 +0530

    Removing unneeded APIs (as of now).

--- client/src/time/include/time.h
+++ client/src/time/include/time.h
@@ -7,11 +7,6 @@ struct Timer
 
     void *obj;
 
-	void (*init_timer)(Timer*);
-	char (*expired)(Timer*);
-	void (*countdown_ms)(Timer*, unsigned int);
-	void (*countdown)(Timer*, unsigned int);
-	int (*left_ms)(Timer*);
 };
 
 Timer* get_new_timer();
--- client/src/time/sys_timeval.c
+++ client/src/time/sys_timeval.c
@@ -27,59 +27,11 @@ struct TimerObj {
 };
 #define GET_IMPLEMENTATION_SPECIFIC_TIMER_OBJ(timer) ((struct TimerObj*)(timer->obj))
 
-char expired(Timer* timer)
-{
-	struct timeval now, res;
-	gettimeofday(&now, NULL);
-	timersub(&GET_IMPLEMENTATION_SPECIFIC_TIMER_OBJ(timer)->end_time, &now, &res);
-	return res.tv_sec < 0 || (res.tv_sec == 0 && res.tv_usec <= 0);
-}
-
-
-void countdown_ms(Timer* timer, unsigned int timeout)
-{
-	struct timeval now;
-	gettimeofday(&now, NULL);
-	struct timeval interval = {timeout / 1000, (timeout % 1000) * 1000};
-	timeradd(&now, &interval, &GET_IMPLEMENTATION_SPECIFIC_TIMER_OBJ(timer)->end_time);
-}
-
-
-void countdown(Timer* timer, unsigned int timeout)
-{
-	struct timeval now;
-	gettimeofday(&now, NULL);
-	struct timeval interval = {timeout, 0};
-	timeradd(&now, &interval, &GET_IMPLEMENTATION_SPECIFIC_TIMER_OBJ(timer)->end_time);
-}
-
-
-int left_ms(Timer* timer)
-{
-	struct timeval now, res;
-	gettimeofday(&now, NULL);
-	timersub(&GET_IMPLEMENTATION_SPECIFIC_TIMER_OBJ(timer)->end_time, &now, &res);
-	return (res.tv_sec < 0) ? 0 : res.tv_sec * 1000 + res.tv_usec / 1000;
-}
-
-
-void init_timer(Timer* timer)
-{
-	GET_IMPLEMENTATION_SPECIFIC_TIMER_OBJ(timer)->end_time = (struct timeval){0, 0};
-}
-
 
 Timer* get_new_timer()
 {
 	Timer *timer = (Timer*) malloc(sizeof(Timer));
 	timer->obj = malloc(sizeof(struct TimerObj));
-
-	timer->init_timer = init_timer;
-	timer->expired = expired;
-	timer->countdown_ms = countdown_ms;
-	timer->countdown = countdown;
-	timer->left_ms = left_ms;
-
 }
 
 void release_timer(Timer* timer)
