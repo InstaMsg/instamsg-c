75fa732dab43a9ae6b9e4b5d27e0ed0494fdc566
Ajay Garg <ajay.garg@sensegrow.com>
2015-08-03 06:23:27 +0530

    Adding environment for flashing code on device (this works at least for Tiva C Series Evaluation Board).
    All that is needed, is
    
    cd embedded-flash-environment
    <Download the file as per README.md>
    sh setup.sh

---
+++ embedded-flash-environment/README.md
@@ -0,0 +1,5 @@
+1)
+Download the file at https://docs.google.com/uc?export=download&confirm=-yjC&id=0B7AGc0Q4gxldV0J5YVF3b3I3UE0 to current-directory.
+
+2)
+sh setup.sh
---
+++ embedded-flash-environment/setup.sh
@@ -0,0 +1,45 @@
+# All courtesy the GOD at http://chrisrm.com/howto-develop-on-the-ti-tiva-launchpad-using-linux/
+
+sudo apt-get update
+sudo apt-get install flex bison libgmp3-dev libmpfr-dev libncurses5-dev libmpc-dev autoconf texinfo build-essential libftdi-dev python-yaml zlib1g-dev libtool
+# I needed these as well flashing over USB
+sudo apt-get install libusb-1.0-0 libusb-1.0-0-dev
+
+
+sudo rm -rf gcc-arm-none-eabi-4_9-2015q2
+tar -xvf gcc-arm-none-eabi-4_9-2015q2-20150609-linux.tar.bz2 -C .
+
+
+. ./Env.ini
+
+
+mkdir -p tivaware
+cd tivaware
+cp ../SW-TM4C-2.1.1.71.exe .
+unzip SW-TM4C-2.1.1.71.exe
+make
+cd ..
+
+
+git clone git@github.com:uctools/tiva-template
+
+
+git clone git://github.com/utzig/lm4tools.git
+cd lm4tools/lm4flash/
+make
+cd ../..
+
+
+git clone http://openocd.zylin.com/openocd
+cd openocd
+git fetch http://openocd.zylin.com/openocd refs/changes/63/2063/1
+git checkout FETCH_HEAD
+git submodule init
+git submodule update
+./bootstrap
+./configure --enable-ti-icdi --prefix=`pwd`/..
+make -j3
+make install
+cd ..
+
+
