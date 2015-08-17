70af42ec8c68d81fa4e84a25c37a7d933f2568ae
Ajay Garg <ajay.garg@sensegrow.com>
2015-08-17 17:36:01 +0530

    checkpoint 3

--- MQTTPacket/src/MQTTPublish.h
+++ MQTTPacket/src/MQTTPublish.h
@@ -46,4 +46,5 @@ DLLExport int MQTTSerialize_puback(unsigned char* buf, int buflen, unsigned shor
 DLLExport int MQTTSerialize_pubrel(unsigned char* buf, int buflen, unsigned char dup, unsigned short packetid);
 DLLExport int MQTTSerialize_pubcomp(unsigned char* buf, int buflen, unsigned short packetid);
 
+DLLExport int MQTTDeserialize_FixedHeaderAndMsgId(MQTTFixedHeaderPlusMsgId *fixedHeaderPlusMsgId, unsigned char* buf, int buflen);
 #endif /* MQTTPUBLISH_H_ */
