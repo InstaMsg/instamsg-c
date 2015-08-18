8440b3a0a9b38c27b774f883c64a8c5f5297c817
Ajay Garg <ajay.garg@sensegrow.com>
2015-08-18 21:27:52 +0530

    Remarks for system-module.

--- client/src/system/ar501_system.c
+++ client/src/system/ar501_system.c
@@ -10,21 +10,44 @@
 
 #include "instamsg_vendor.h"
 
+
+/* This method fills in the string-value of device-manufacturer in "buf".
+ *
+ * Note that the "buf" will be all-0-initialized from the callee, so the vendor-implementation
+ * does not need to bother about that.
+ *
+ * Also, the device-implementation must take care that the manufacturer-value (including null-terminator)
+ * is not greater than "maxValueLenAllowed".
+*/
 static void getManufacturer(System *system, char *buf, int maxValueLenAllowed)
 {
 }
 
 
+/* This method fills in the string-value of device-serial-number in "buf".
+ *
+ * Note that the "buf" will be all-0-initialized from the callee, so the vendor-implementation
+ * does not need to bother about that.
+ *
+ * Also, the device-implementation must take care that the serial-number-value (including null-terminator)
+ * is not greater than "maxValueLenAllowed".
+*/
 static void getSerialNumber(System *system, char *buf, int maxValueLenAllowed)
 {
 }
 
 
+/*
+ * This method reboots the device, thus completely re-initializing the system.
+ */
 static void rebootDevice(System *sys)
 {
 }
 
 
+/*
+ * NOTHING EXTRA NEEDS TO BE DONE HERE.
+ */
 void init_system_utils(System *system, void *arg)
 {
     system->getManufacturer = getManufacturer;
@@ -33,11 +56,18 @@ void init_system_utils(System *system, void *arg)
 }
 
 
+/*
+ * NOTHING EXTRA NEEDS TO BE DONE HERE.
+ */
 void release_system_utils(System *system)
 {
 }
 
 
+/*
+ * This method does the one-time system-initialization.
+ * This code is called right at the beginning of InstaMsg-binary.
+ */
 void SYSTEM_GLOBAL_INIT()
 {
 }
