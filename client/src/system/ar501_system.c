1bf315b02776c7df9694d58bd54662468775f341
Ajay Garg <ajay.garg@sensegrow.com>
2015-08-18 21:20:45 +0530

    Cleaning

--- client/src/system/ar501_system.c
+++ client/src/system/ar501_system.c
@@ -9,18 +9,14 @@
 #include <string.h>
 
 #include "instamsg_vendor.h"
-#include "atoll_common.h"
-#include "atoll_gsm.h"
 
 static void getManufacturer(System *system, char *buf, int maxValueLenAllowed)
 {
-    snprintf(buf, maxValueLenAllowed, "ATOLL");
 }
 
 
 static void getSerialNumber(System *system, char *buf, int maxValueLenAllowed)
 {
-    snprintf(buf, maxValueLenAllowed, "RANDOM-ATOLL-SERIAL");
 }
 
 
@@ -44,23 +40,4 @@ void release_system_utils(System *system)
 
 void SYSTEM_GLOBAL_INIT()
 {
-    AtollrunningFrq_init();
-
-    /*inetialise uart 0 for 115200 baud rate with interrupt module*/
-    AtollUart0_Init(115200,1);
-
-    /*send a string"UART0 TEST" to uart 0*/
-    AtollUart0_StringSend("\r\nUART0 TEST\0");
-
-    /*inetialise uart 6 for 115200 baud rate with interrupt module*/
-    AtollUart6_Init(115200,1);
-
-    /*send a string"UART6 TEST" to uart 6*/
-    AtollUart6_StringSend("\r\nUART6 TEST\0");
-
-    /*initialize the 4 digital input */
-    Atoll_gpio_input_init(1,1,1,1);
-
-    /* initialize the gsm peripharal*/
-    AtollGSMperipheral_init();
 }
