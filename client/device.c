30cb6d2ca13db986a35b5028a683ecee4a562f4e
Ajay Garg <ajay.garg@sensegrow.com>
2015-09-12 18:48:35 +0530

    Adding publisher-logic in business-logic section (for cross-testing the connectivity of devices across realms).
    A message published from AR501-board (as publisher) was received on a Linux-desktop (as subscriber) !!!!!
    :) :) :) :)

--- client/device.c
+++ client/device.c
@@ -3,9 +3,33 @@
 
 #include <string.h>
 
+static void publishAckReceived(MQTTFixedHeaderPlusMsgId *fixedHeaderPlusMsgId)
+{
+    info_log("PUBACK received for msg-id [%u]", fixedHeaderPlusMsgId->msgId);
+}
+
+
+static char msgBuf[10];
+const char *topic = "listener_topic";
+
 void coreLoopyBusinessLogicInitiatedBySelf()
 {
-    info_log("Business-Logic Called.");
+    int rc;
+
+    startAndCountdownTimer(3, 0);
+	if(1)
+	{
+        memset(msgBuf, 0, 10);
+
+        static long counter;
+        counter++;
+        sg_sprintf(msgBuf, "Test %u", counter);
+
+		info_log("Publishing message [%s] to %s", msgBuf, topic);
+		rc = MQTTPublish(&instaMsg, topic, (const char*)msgBuf, 2, 0,
+                         publishAckReceived, MQTT_RESULT_HANDLER_TIMEOUT, 0, 1);
+		info_log("Published %d", rc);
+	}
 }
 
 
