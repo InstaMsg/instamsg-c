9b361cf2d129b2332e51ae7fd85461b11a8f47d2
Ajay Garg <ajay.garg@sensegrow.com>
2015-09-26 00:29:11 +0530

    More APIs-documentation

--- client/src/driver/include/serial_logger.h
+++ client/src/driver/include/serial_logger.h
@@ -3,11 +3,27 @@
 
 #include "device_serial_logger.h"
 
+/*
+ * Global-functions callable.
+ */
 void init_serial_logger_interface(SerialLoggerInterface *serialLoggerInterface, void *arg);
 void release_serial_logger_interface(SerialLoggerInterface *serialLoggerInterface);
 
-void connect_underlying_serial_logger_medium_guaranteed(SerialLoggerInterface *serialLoggerInterface);
+
+/*
+ * Must not be called directly.
+ * Instead must be called as ::
+ *
+ *      serialLoggerInterface->write
+ */
 int serial_logger_write(SerialLoggerInterface* serialLoggerInterface, unsigned char* buffer, int len);
+
+
+/*
+ * Internally Used.
+ * Must not be called by anyone.
+ */
+void connect_underlying_serial_logger_medium_guaranteed(SerialLoggerInterface *serialLoggerInterface);
 void release_underlying_serial_logger_medium_guaranteed(SerialLoggerInterface *serialLoggerInterface);
 
 #endif
