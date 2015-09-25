65c16b8441eb0057207aabfd1d5869e4f00344b0
Ajay Garg <ajay.garg@sensegrow.com>
2015-09-26 00:17:32 +0530

    Updating APIs-documentation

--- client/src/driver/include/file_system.h
+++ client/src/driver/include/file_system.h
@@ -5,17 +5,40 @@
 
 #include "device_file_system.h"
 
+/*
+ * Global-functions callable.
+ */
 void init_file_system(FileSystem *fs, void *arg);
 void release_file_system(FileSystem *fs);
 
+
+/*
+ * Internally Used.
+ * Must not be called by anyone.
+ */
 void connect_underlying_file_system_medium_guaranteed(FileSystem* fs);
+void release_underlying_file_system_medium_guaranteed(FileSystem* fs);
+
+
+/*
+ * Must not be called directly.
+ * Instead must be called as ::
+ *
+ *      fs->read
+ *      fs->write
+ *      fs->renameFile
+ *      fs->deleteFile
+ *      fs->getFileListing
+ *      fs->getFileSize
+ */
 int file_system_read (FileSystem *fs, unsigned char* buffer, int len, unsigned char guaranteed);
 int file_system_write(FileSystem *fs, unsigned char* buffer, int len);
 int renameFile(FileSystem *fs, const char *oldPath, const char *newPath);
 int deleteFile(FileSystem *fs, const char *filePath);
 void getFileListing(FileSystem *fs, char *buf, int maxValueLenAllowed, const char *directoryPath);
 long getFileSize(FileSystem *fs, const char *filepath);
-void release_underlying_file_system_medium_guaranteed(FileSystem* fs);
+
+
 
 #endif
 
--- client/src/driver/include/network.h
+++ client/src/driver/include/network.h
@@ -3,18 +3,34 @@
 
 #include "device_network.h"
 
+/*
+ * Global-functions callable.
+ */
 void init_network(Network *network, const char *hostName, unsigned int port);
-void release_network(Network *network);
+void release_underlying_network_medium_guaranteed(Network* network);
 
+
+/*
+ * Internally Used.
+ * Must not be called by anyone.
+ */
+void release_network(Network *network);
 #ifdef GSM_INTERFACE_ENABLED
 void get_latest_sms_containing_substring(Network *network, char *buffer, const char *prefix);
 #endif
-
 void get_device_uuid(Network *network, char *buffer);
 void connect_underlying_network_medium_try_once(Network* network);
+
+
+/*
+ * Must not be called directly.
+ * Instead must be called as ::
+ *
+ *      network->read
+ *      network->write
+ */
 int network_read (Network *network, unsigned char* buffer, int len, unsigned char guaranteed);
 int network_write(Network *network, unsigned char* buffer, int len);
-void release_underlying_network_medium_guaranteed(Network* network);
 
 
 #endif
