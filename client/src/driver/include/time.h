b942157a7f809c46596ad68d9a9aa008dcbc0a1b
Ajay Garg <ajay.garg@sensegrow.com>
2015-09-26 00:33:35 +0530

    API-docs for Timer-interface

--- client/src/driver/include/time.h
+++ client/src/driver/include/time.h
@@ -3,12 +3,29 @@
 
 #include "device_time.h"
 
+/*
+ * Global-functions callable.
+ */
 void init_timer(Timer *timer, void *arg);
 void release_timer(Timer* timer);
 
-void connect_underlying_time_medium_guaranteed(Timer *timer);
+
+/*
+ * Must not be called directly.
+ * Instead must be called as ::
+ *
+ *      timer->getMinimumDelayPossibleInMicroSeconds
+ *      timer->minimumDelay
+ */
 unsigned int getMinimumDelayPossibleInMicroSeconds(Timer *timer);
 void minimumDelay(Timer *timer);
+
+
+/*
+ * Internally Used.
+ * Must not be called by anyone.
+ */
+void connect_underlying_time_medium_guaranteed(Timer *timer);
 void release_underlying_time_medium_guaranteed(Timer* timer);
 
 Timer singletonUtilityTimer;
