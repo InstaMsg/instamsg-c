0c0d0851b48cf02e1362e481a80ed46487b3c501
Ajay Garg <ajay.garg@sensegrow.com>
2015-09-27 17:09:41 +0530

    Driver function for ioeye.. need implementations.

--- ioeye/main.c
+++ ioeye/main.c
@@ -1,45 +1,40 @@
 #include "../instamsg/driver/include/instamsg.h"
 #include "../instamsg/driver/include/globals.h"
 
-#include <string.h>
 
-static void publishAckReceived(MQTTFixedHeaderPlusMsgId *fixedHeaderPlusMsgId)
+static int sendClientSessionData()
 {
-    info_log("PUBACK received for msg-id [%u]", fixedHeaderPlusMsgId->msgId);
+    return SUCCESS;
 }
 
 
-static char msgBuf[10];
-const char *topic = "listener_topic";
+static int sendClientMetaData()
+{
+    return SUCCESS;
+}
+
+
+static int onConnect()
+{
+    int rc = FAILURE;
+
+    rc = sendClientSessionData();
+    rc = sendClientMetaData();
+
+    return rc;
+}
+
 
 void coreLoopyBusinessLogicInitiatedBySelf()
 {
-    int rc;
-
-    startAndCountdownTimer(3, 0);
-	if(1)
-	{
-        memset(msgBuf, 0, 10);
-
-        static long counter;
-        counter++;
-        sg_sprintf(msgBuf, "Test %u", counter);
-
-		info_log("Publishing message [%s] to %s", msgBuf, topic);
-		rc = MQTTPublish(&instaMsg, topic, (const char*)msgBuf, 2, 0,
-                         publishAckReceived, MQTT_RESULT_HANDLER_TIMEOUT, 0);
-		info_log("Published %d", rc);
-
-#ifdef DEBUG_MODE
-        startAndCountdownTimer(300, 1);
-#endif
-	}
+    onConnect();
+    startAndCountdownTimer(300, 1);
 }
 
 
 int main(int argc, char** argv)
 {
     globalSystemInit();
-    start(NULL, NULL, NULL, coreLoopyBusinessLogicInitiatedBySelf, NULL);
+    start(onConnect, NULL, NULL, coreLoopyBusinessLogicInitiatedBySelf, NULL);
 }
 
