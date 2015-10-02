9c9d7cf45f2160ed5b7c8aa2d093eb4406226a2e
Ajay Garg <ajay.garg@sensegrow.com>
2015-10-02 22:29:36 +0530

    Ioeye-time stubs

---
+++ device/stub/ioeye/device_time.c
@@ -0,0 +1,17 @@
+/*
+ * This method gets the time in the following-format
+ *
+ *          YYYYMMDD4HHMMSS
+ *          201507304155546
+ */
+void getTimeInDesiredFormat(char *buffer)
+{
+}
+
+
+/*
+ * This method gets the timezone-offset for this device.
+ */
+void getTimezoneOffset(char *buffer)
+{
+}
---
+++ ioeye/include/time.h
@@ -0,0 +1,7 @@
+#ifndef IOEYE_TIME_COMMON
+#define IOEYE_TIME_COMMON
+
+void getTimeInDesiredFormat(char *buffer);
+void getTimezoneOffset(char *buffer);
+
+#endif
