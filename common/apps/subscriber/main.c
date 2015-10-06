e34e10bcee8f62869662d8365eb2705b0c544547
Ajay Garg <ajay.garg@sensegrow.com>
2015-10-06 20:20:01 +0530

    More doc'ing

--- common/apps/subscriber/main.c
+++ common/apps/subscriber/main.c
@@ -1,3 +1,10 @@
-int main()
+#include "../../instamsg/driver/include/instamsg.h"
+
+static void coreLoopyBusinessLogicInitiatedBySelf()
 {
+}
+
+int main(int argc, char** argv)
+{
+    start(NULL, NULL, NULL, coreLoopyBusinessLogicInitiatedBySelf, 1);
 }
