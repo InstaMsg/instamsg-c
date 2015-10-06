92f21cba256a47489e19052400a4dfe50781dd14
Ajay Garg <ajay.garg@sensegrow.com>
2015-10-06 20:27:12 +0530

    Publisher logic complete !!

--- common/apps/publisher/main.c
+++ common/apps/publisher/main.c
@@ -1,8 +1,29 @@
 #include "../../instamsg/driver/include/instamsg.h"
 
+static void publishAckReceived(MQTTFixedHeaderPlusMsgId *fixedHeaderPlusMsgId)
+{
+    info_log("PUBACK received for msg-id [%u]", fixedHeaderPlusMsgId->msgId);
+}
+
+
 static void coreLoopyBusinessLogicInitiatedBySelf()
 {
+    char buf[20] = {0};
+
+    static int counter;
+    counter++;
+    sprintf(buf, "Test %d", counter);
+
+    MQTTPublish(&instaMsg,
+                "listener_topic",
+                buf, QOS2,
+                0,
+                publishAckReceived,
+                MQTT_RESULT_HANDLER_TIMEOUT,
+                0,
+                1);
 }
+
 
 int main(int argc, char** argv)
 {
