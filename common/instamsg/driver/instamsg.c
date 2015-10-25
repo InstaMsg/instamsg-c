64f25def2b638eae9ae6f12ba1d7fe5541ceb2fa
Ajay Garg <ajay.garg@sensegrow.com>
2015-10-25 16:02:52 +0530

    instamsg-kernel-common: whenever start() method is called, clear the previous "version" of instamsg .. it doesn't harm.
    Particularly useful, if the app wants to (re)start instamsg, depending
    on some particular condition.

--- common/instamsg/driver/instamsg.c
+++ common/instamsg/driver/instamsg.c
@@ -1388,6 +1388,11 @@ void start(int (*onConnectOneTimeOperations)(),
 
     while(1)
     {
+        if(c->connected == 1)
+        {
+            clearInstaMsg(c);
+        }
+
         initInstaMsg(c, onConnectOneTimeOperations, onDisconnect, oneToOneMessageReceivedHandler);
 
         RESET_GLOBAL_BUFFER;
