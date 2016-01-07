de4aecc5280713732cad2612ee2b0941621eaddb
Ajay Garg <ajay.garg@sensegrow.com>
2016-01-07 20:10:06 +0530

    now, just the end-to-end media-streaming-and-transfer TESTING remains :)

--- device/linux_desktop/instamsg/device_media.c
+++ device/linux_desktop/instamsg/device_media.c
@@ -8,6 +8,42 @@
 #include <gst/gst.h>
 
 
+static void *startPipeline(void *arg)
+{
+    GstElement *pipeline;
+    GstBus *bus;
+    GstMessage *msg;
+
+    gst_init (NULL, NULL);
+    RESET_GLOBAL_BUFFER;
+    sg_sprintf((char*) GLOBAL_BUFFER, "v4l2src device=/dev/video0 ! udpsink host=%s port=%s", mediaServerIpAddress, mediaServerPort);
+
+    pipeline = gst_parse_launch ((char*) GLOBAL_BUFFER, NULL);
+    gst_element_set_state (pipeline, GST_STATE_PLAYING);
+
+
+    /*
+     * Wait until error or EOS.
+     */
+    bus = gst_element_get_bus (pipeline);
+    msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+
+    /*
+     * If code reaches here, it means the media-loop had some error occurred.
+     */
+    mediaStreamingErrorOccurred = 1;
+
+    /*
+     * Free resources
+     */
+    if (msg != NULL)
+        gst_message_unref (msg);
+
+    gst_object_unref (bus);
+    gst_element_set_state (pipeline, GST_STATE_NULL);
+    gst_object_unref (pipeline);
+}
+
 
 /*
  * This method starts streaming, to the desired media-server.
@@ -21,15 +57,8 @@
  */
 void create_and_start_streaming_pipeline(const char *mediaServerIpAddress, const char *mediaServerPort)
 {
-    GstElement *pipeline;
-
-    gst_init (NULL, NULL);
-
-    RESET_GLOBAL_BUFFER;
-    sg_sprintf((char*) GLOBAL_BUFFER, "v4l2src device=/dev/video0 ! udpsink host=%s port=%s", mediaServerIpAddress, mediaServerPort);
-
-    pipeline = gst_parse_launch ((char*) GLOBAL_BUFFER, NULL);
-    gst_element_set_state (pipeline, GST_STATE_PLAYING);
+    pthread_t tid;
+    pthread_create(&tid, NULL, startPipeline, NULL);
 }
 
 
