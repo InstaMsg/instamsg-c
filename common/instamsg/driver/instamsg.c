25b3d22b9af8ded2c6b6de2fe92db5c1976595af
Ajay Garg <ajay.garg@sensegrow.com>
2016-02-11 17:13:13 +0530

    adding some time, before sending notification-message.

--- common/instamsg/driver/instamsg.c
+++ common/instamsg/driver/instamsg.c
@@ -1517,6 +1517,8 @@ static void handleConnOrProvAckGeneric(InstaMsg *c, int connack_rc)
 
         if(notifyServerOfSecretReceived == 1)
         {
+            startAndCountdownTimer(5, 0);
+
             mqttConnectFlag = 1;
             publish(NOTIFICATION_TOPIC,
                     "SECRET RECEIVED",
