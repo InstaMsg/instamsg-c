20d566e4b5c52ec77739e5ab203832c852551746
Ajay Garg <ajay.garg@sensegrow.com>
2016-02-25 16:05:56 +0530

    changing default definition

--- device/linux_desktop/ioeye/device_modbus.h
+++ device/linux_desktop/ioeye/device_modbus.h
@@ -2,20 +2,25 @@
 #define IOEYE_MODBUS
 
 #include "../../../common/instamsg/driver/include/globals.h"
+#include "../../../common/ioeye/include/globals.h"
 
 typedef struct Modbus Modbus;
 
 struct Modbus
 {
     /* ============================= THIS SECTION MUST NOT BE TEMPERED ==================================== */
-    int interfaceNumber;
+    MODBUS_DEVICE_TYPE deviceType;
+    const char *identifier;
     int (*send_command_and_read_response_sync)(Modbus *modbus,
                                                unsigned char *commandBytes,
                                                int commandBytesLength,
                                                unsigned char *responseByteBuffer,
                                                int responseBytesLength);
 
-    char modbusCommands[MAX_BUFFER_SIZE];
+    char modbusCommands[MODBUS_COMMAND_BUFFER_SIZE];
+
+    short (*shortPayloadValueGetter)(void *arg);
+    void *shortPayloadValueGetterArg;
     /* ============================= THIS SECTION MUST NOT BE TEMPERED ==================================== */
 
 
