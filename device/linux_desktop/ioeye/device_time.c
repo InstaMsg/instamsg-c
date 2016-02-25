ad42c483eb93c29c68978817b52f2bb61135eeeb
Ajay Garg <ajay.garg@sensegrow.com>
2016-02-25 15:41:37 +0530

    adding ioeye-time implementations for linux-like systems

--- device/linux_desktop/ioeye/device_time.c
+++ device/linux_desktop/ioeye/device_time.c
@@ -1,3 +1,5 @@
+#include <time.h>
+
 /*
  * This method gets the time in the following-format
  *
@@ -6,6 +8,13 @@
  */
 void getTimeInDesiredFormat(char *buffer, int maxBufferLength)
 {
+    time_t rawtime;
+    struct tm *info;
+
+    time(&rawtime);
+    info = localtime(&rawtime);
+
+    strftime(buffer, maxBufferLength,"%Y%m%d4%H%M%S", info);
 }
 
 
@@ -14,4 +23,5 @@ void getTimeInDesiredFormat(char *buffer, int maxBufferLength)
  */
 void getTimezoneOffset(char *buffer, int maxBufferLength)
 {
+    strcpy(buffer, "19800");
 }
