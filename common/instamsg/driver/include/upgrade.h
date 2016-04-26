87b6936680057a4842f2d9bdb0ddf95dfd58cefe
Ajay Garg <ajay.garg@sensegrow.com>
2016-04-26 18:15:03 +0530

    adding declaration in header-file, so as to avoid warnings

--- common/instamsg/driver/include/upgrade.h
+++ common/instamsg/driver/include/upgrade.h
@@ -11,5 +11,7 @@ void tear_down_binary_download();
 
 void check_for_upgrade();
 void upgrade_executable_binary();
+void remove_old_executable_binary();
+void copy_new_executable_binary_from_temp_location();
 
 #endif
