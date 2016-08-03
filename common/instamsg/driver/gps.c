d500c902da4a4185643d87f8f9ed127940a56334
Ajay <ajay.garg@sensegrow.com>
2016-08-03 13:46:33 +0530

    fixing a crash-situation

--- common/instamsg/driver/gps.c
+++ common/instamsg/driver/gps.c
@@ -25,9 +25,15 @@ void trim_buffer_to_contain_only_first_GPRMC_sentence(unsigned char *buffer, int
             {
                 break;
             }
+
+            buffer++;
+            i++;
         }
-        buffer++;
-        i++;
+        else
+        {
+        	buffer[0] = 0;
+        	return;
+        }
     }
 
     /*
