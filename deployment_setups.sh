666e05e4cdbd58675d22a58416d359f08494636d
Ajay Garg <ajay.garg@sensegrow.com>
2016-08-05 00:11:45 +0530

    updating setups

--- deployment_setups.sh
+++ deployment_setups.sh
@@ -4,3 +4,6 @@ zip -r pi_setup.zip device/pi/setup
 rm -f maestro_e220_setup.zip
 zip -r maestro_e220_setup.zip device/maestro_e220/setup
 
+rm -f lanner_pc_setup.zip
+zip -r lanner_pc_setup.zip device/lanner_pc/setup
+
