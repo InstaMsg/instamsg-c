f0af1417a195b559d588a7cf103a32848c5da8d8
Ajay Garg <ajay.garg@sensegrow.com>
2016-08-30 19:15:19 +0530

    updating

--- deployment_setups.sh
+++ deployment_setups.sh
@@ -6,3 +6,4 @@ zip -r maestro-e220_setup.zip device/maestro-e220/setup
 
 rm -f lanner-pc_setup.zip
 zip -r lanner-pc_setup.zip device/lanner-pc/setup
+mv lanner-pc_setup.zip common/ioeye/binaries/energy-meter
