5b5faa201227f6c8164aa16bee14ae43ea153868
Ajay Garg <ajay.garg@sensegrow.com>
2016-10-03 14:56:30 +0530

    adding signal, imsi, imei values for maestro-e220

--- setups/maestro-e220/monitor.sh
+++ setups/maestro-e220/monitor.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+move_file()
+{
+    mv /overlay/home/sensegrow/at_temp /overlay/home/sensegrow/$1
+}
+
+
 while true
 do
     PID=`ps | grep instamsg | grep -v grep | grep -v tail | sed -e 's/^ *//g' | cut -d\  -f 1`
@@ -15,6 +21,15 @@ do
     else
 	    echo "Binary running fine"
     fi
+
+    /usr/bin/sendat /dev/ttyACM4 "at+csq" 2 | grep -v csq | grep -v OK | grep -v "^$" | head -1 | cut -d: -f 2 | cut -d, -f 1 > /overlay/home/sensegrow/at_temp
+    move_file "signal"
+
+    /usr/bin/sendat /dev/ttyACM4 "at+cgsn" 2 | grep -v cgsn | grep -v OK | grep -v "^$" > /overlay/home/sensegrow/at_temp
+    move_file "imei"
+
+    /usr/bin/sendat /dev/ttyACM4 "at+cimi" 2 | grep -v cimi | grep -v OK | grep -v "^$" > /overlay/home/sensegrow/at_temp
+    move_file "imsi"
 
     sleep 60
 done
