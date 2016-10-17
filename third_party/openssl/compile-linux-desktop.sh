08e3984e5f334d7ee59724110759aabe4568fbbc
Ajay Garg <ajay.garg@sensegrow.com>
2016-10-17 12:07:14 +0530

    disabling some conditional-compilations for openssl.

--- third_party/openssl/compile-linux-desktop.sh
+++ third_party/openssl/compile-linux-desktop.sh
@@ -1,9 +1,28 @@
 set -e
 set -x
 
-./Configure no-ec linux-elf
+# Enable/Disable features via https://gist.github.com/steakknife/8247726
+#
+./Configure                     \
+                no-ec           \
+                no-ecdsa        \
+                no-ecdh         \
+                no-ec2m         \
+                no-mdc2         \
+                no-idea         \
+                no-capieng      \
+                no-bf           \
+                no-camellia     \
+                no-whirlpool    \
+                no-ssl2         \
+                no-threads      \
+                no-krb5         \
+    linux-elf
+
 make depend
 make
 sudo cp libssl.a /usr/lib/i386-linux-gnu
 sudo cp libcrypto.a /usr/lib/i386-linux-gnu
+ls -lrt /usr/lib/i386-linux-gnu/libssl.a
+ls -lrt /usr/lib/i386-linux-gnu/libcrypto.a
 
