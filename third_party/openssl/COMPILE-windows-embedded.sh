9ca1dc74b29120da194cb7848da205bb00325f58
Ajay Garg <ajay.garg@sensegrow.com>
2016-11-09 14:30:06 +0530

    less strains to eyes

--- third_party/openssl/COMPILE-windows-embedded.sh
+++ third_party/openssl/COMPILE-windows-embedded.sh
@@ -1,2 +1,8 @@
+echo
+echo
+echo "1."
 echo "WINDOWS is EVIL. Even OpenSSL cannot compile on it."
-echo "So, please link the pre-placed libraries libMT.pdb, libeay32MT.lib, ssleay32MT.lib (placed in instamsg folder)"
+echo
+echo
+echo "2."
+echo "So, please link the pre-placed libraries libMT.pdb, libeay32MT.lib, ssleay32MT.lib (placed in instamsg folder), to your project."
