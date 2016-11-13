05f975b6058b89ddfaff571becef51013d265e6b
Ajay Garg <ajay.garg@sensegrow.com>
2016-11-14 01:03:02 +0530

    updating binaries

---
+++ setups/lanner-pc/upgrade-from-301-to-302-without-internet.sh
@@ -0,0 +1,14 @@
+set -x
+set -e
+
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash intel_idle.max_cstate=1"/g' /etc/default/grub
+update-grub
+
+kill -9 `pgrep -x instamsg` || true
+cp energy-meter_lanner-pc_3.0.2_3.0.2 /home/sensegrow/instamsg
+
+echo
+echo
+echo "PLEASE REBOOT THE MACHINE NOW ..."
+echo
+
