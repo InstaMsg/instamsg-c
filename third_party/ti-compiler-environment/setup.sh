956bf079c25db1e392fb30e8548d9954c1532954
deepak_sg <deepak.agrawal@sensegrow.com>
2016-11-25 12:09:08 +0530

    arm-gcc-eabi compilation for fresh start

--- third_party/ti-compiler-environment/setup.sh
+++ third_party/ti-compiler-environment/setup.sh
@@ -2,6 +2,11 @@
 
 # Some repositories might not be updatable, so using "set -e" from here afterwards.
 sudo apt-get update
+sudo rm -r tivaware
+sudo rm -r tiva-template
+sudo rm -r lm4tools
+sudo rm -r openocd
+sudo rm -rf gcc-arm-none-eabi-4_9-2015q2
 
 set -e
 
@@ -10,13 +15,11 @@ sudo apt-get install flex bison libgmp3-dev libmpfr-dev libncurses5-dev libmpc-d
 sudo apt-get install libusb-1.0-0 libusb-1.0-0-dev
 
 
-sudo rm -rf gcc-arm-none-eabi-4_9-2015q2
 tar -xvf gcc-arm-none-eabi-4_9-2015q2-20150609-linux.tar.bz2 -C .
 
 
 export PATH=$PATH:$PWD/../ti-compiler-environment/gcc-arm-none-eabi-4_9-2015q2/bin
 
-sudo rm -r tivaware
 mkdir -p tivaware
 cd tivaware
 cp ../SW-TM4C-2.1.1.71.exe .
@@ -25,18 +28,15 @@ make
 cd ..
 
 
-sudo rm -r tiva-template
 git clone git@github.com:uctools/tiva-template
 
 
-sudo rm -r lm4tools
 git clone git://github.com/utzig/lm4tools.git
 cd lm4tools/lm4flash/
 make
 cd ../..
 
 
-sudo rm -r openocd
 git clone http://openocd.zylin.com/openocd
 cd openocd
 git fetch http://openocd.zylin.com/openocd refs/changes/63/2063/1
