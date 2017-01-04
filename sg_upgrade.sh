cb3629bceb63deb79ef734c5223bb53aadf4380c
Ajay Garg <ajay.garg@sensegrow.com>
2017-01-04 09:48:32 +0530

    checking package-versions before upgrading

--- sg_upgrade.sh
+++ sg_upgrade.sh
@@ -14,6 +14,29 @@ mv setup.zip* setup.zip
 unzip setup.zip -d .
 
 cd setup
-chmod 777 ./sg_setup.sh
-./sg_setup.sh
+
+prev_version=`cat "${HOME_DIRECTORY}/current_version"`
+new_version=`cat ./sg_setup.sh | grep "PKG_VERSION" | cut -d\" -f 2`
+
+if [ -z "${prev_version}" ]
+then
+    echo "Current-Version not available ... aborting .."
+    exit
+fi
+
+if [ -z "${new_version}" ]
+then
+    echo "New Package-Version not available ... aborting .."
+    exit
+fi
+
+if [ "${new_version}" -gt "${prev_version}" ]
+then
+    chmod 777 ./sg_setup.sh
+    ./sg_setup.sh
+
+    echo "${new_version}" > "${HOME_DIRECTORY}/current_version"
+else
+    echo "No new version available !!"
+fi
 
