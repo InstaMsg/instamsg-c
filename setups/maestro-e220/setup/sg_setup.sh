77ccaf5ad3e501a646b74bd1f7ee9099359bb954
Ajay Garg <ajay.garg@sensegrow.com>
2017-01-12 18:32:04 +0530

    updating binary for maestro-e220 (plus its associated upgrade-setup)

--- setups/maestro-e220/setup/sg_setup.sh
+++ setups/maestro-e220/setup/sg_setup.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 . ../upgrade_params
-PKG_VERSION="1"
+PKG_VERSION="2"
 
 ###################### PERFORM ACTIONS NOW ##################################
+
+/usr/bin/killall monitor.sh || true
+/usr/bin/killall instamsg || true
+
+cp energy-meter_maestro-e220_3.0.7_3.0.7 /overlay/home/sensegrow/instamsg
+
+/overlay/home/sensegrow/reboot.sh
