c3f30e50369485e425c499551561b84b891f3780
Ajay Garg <ajay.garg@sensegrow.com>
2017-01-13 11:03:53 +0530

    lanner binary upgrade-framework

--- setups/lanner-pc/setup/sg_setup.sh
+++ setups/lanner-pc/setup/sg_setup.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 . ../upgrade_params
-PKG_VERSION="1"
+PKG_VERSION="2"
 
 ###################### PERFORM ACTIONS NOW ##################################
+
+/usr/bin/killall monitor.sh || true
+/usr/bin/killall instamsg || true
+
+cp energy-meter_lanner-pc_3.0.7_3.0.7 /home/sensegrow/instamsg
+
+/sbin/reboot
