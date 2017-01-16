f29b46798c3ddb4942bb36aa795684790187a5a4
Ajay Garg <ajay.garg@sensegrow.com>
2017-01-16 15:41:55 +0530

    minor

--- setups/maestro-e220/setup.sh
+++ setups/maestro-e220/setup.sh
@@ -49,7 +49,7 @@ ${SSH_COMMAND} "chmod 777 ${HOME_DIRECTORY}/sg_upgrade.sh"
 scp upgrade_params ${LOGIN}:${HOME_DIRECTORY}
 ${SSH_COMMAND} "chmod 777 ${HOME_DIRECTORY}/upgrade_params"
 
-${SSH_COMMAND} "echo 3 > ${HOME_DIRECTORY}/current_version"
+${SSH_COMMAND} "echo 4 > ${HOME_DIRECTORY}/current_version"
 
 ${SSH_COMMAND} "killall instamsg"
 scp $1  ${LOGIN}:${HOME_DIRECTORY}/instamsg
