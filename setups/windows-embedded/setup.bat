d1f6cf9163f2e580195c41726c677c46fd458888
Ajay Garg <ajay.garg@sensegrow.com>
2017-02-08 11:56:06 +0530

    using native copy and move commands for windows

--- setups/windows-embedded/setup.bat
+++ setups/windows-embedded/setup.bat
@@ -5,32 +5,32 @@ set HOME_DIRECTORY=c:\sensegrow
 
 mkdir %HOME_DIRECTORY%
 
-cp -f monitor.bat  %HOME_DIRECTORY%
+copy -f monitor.bat  %HOME_DIRECTORY%
 echo test > %HOME_DIRECTORY%\prov.txt
 
-cp -f curl.exe  %HOME_DIRECTORY%
+copy -f curl.exe  %HOME_DIRECTORY%
 
-cp -f upgrade_params.bat  %HOME_DIRECTORY%
-cp -f sg_upgrade_try.bat  %HOME_DIRECTORY%
-cp -f sg_upgrade.bat  %HOME_DIRECTORY%
-cp -f upgrade_params.bat  %HOME_DIRECTORY%
+copy -f upgrade_params.bat  %HOME_DIRECTORY%
+copy -f sg_upgrade_try.bat  %HOME_DIRECTORY%
+copy -f sg_upgrade.bat  %HOME_DIRECTORY%
+copy -f upgrade_params.bat  %HOME_DIRECTORY%
 echo 0 > %HOME_DIRECTORY%\current_version
 
 >>data.txt type nul
 
-cp -f baretail.exe %HOME_DIRECTORY%
+copy -f baretail.exe %HOME_DIRECTORY%
 echo > %HOME_DIRECTORY%\instamsg.log
 
 taskkill /IM monitor.bat /F
 taskkill /IM instamsg.exe /F
 
-cp -f instamsg.lnk "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+copy -f instamsg.lnk "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 
-cp -f %1.exe %HOME_DIRECTORY%
-mv %HOME_DIRECTORY%\%1.exe %HOME_DIRECTORY%\instamsg.exe
-cp -f %1.ipdb %HOME_DIRECTORY%
-mv %HOME_DIRECTORY%\%1.ipdb %HOME_DIRECTORY%\instamsg.ipdb
-cp -f %1.iobj %HOME_DIRECTORY%
-mv %HOME_DIRECTORY%\%1.iobj %HOME_DIRECTORY%\instamsg.iobj
-cp -f %1.pdb %HOME_DIRECTORY%
-mv %HOME_DIRECTORY%\%1.pdb %HOME_DIRECTORY%\instamsg.pdb
+copy -f %1.exe %HOME_DIRECTORY%
+move %HOME_DIRECTORY%\%1.exe %HOME_DIRECTORY%\instamsg.exe
+copy -f %1.ipdb %HOME_DIRECTORY%
+move %HOME_DIRECTORY%\%1.ipdb %HOME_DIRECTORY%\instamsg.ipdb
+copy -f %1.iobj %HOME_DIRECTORY%
+move %HOME_DIRECTORY%\%1.iobj %HOME_DIRECTORY%\instamsg.iobj
+copy -f %1.pdb %HOME_DIRECTORY%
+move %HOME_DIRECTORY%\%1.pdb %HOME_DIRECTORY%\instamsg.pdb
