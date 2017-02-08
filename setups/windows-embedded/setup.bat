61b195a4ca4d573f8eaa7a954aacbc997bae4084
Ajay Garg <ajay.garg@sensegrow.com>
2017-02-08 11:58:40 +0530

    correcting flag

--- setups/windows-embedded/setup.bat
+++ setups/windows-embedded/setup.bat
@@ -5,20 +5,20 @@ set HOME_DIRECTORY=c:\sensegrow
 
 mkdir %HOME_DIRECTORY%
 
-copy -f monitor.bat  %HOME_DIRECTORY%
+copy /Y monitor.bat  %HOME_DIRECTORY%
 echo test > %HOME_DIRECTORY%\prov.txt
 
-copy -f curl.exe  %HOME_DIRECTORY%
+copy /Y curl.exe  %HOME_DIRECTORY%
 
-copy -f upgrade_params.bat  %HOME_DIRECTORY%
-copy -f sg_upgrade_try.bat  %HOME_DIRECTORY%
-copy -f sg_upgrade.bat  %HOME_DIRECTORY%
-copy -f upgrade_params.bat  %HOME_DIRECTORY%
+copy /Y upgrade_params.bat  %HOME_DIRECTORY%
+copy /Y sg_upgrade_try.bat  %HOME_DIRECTORY%
+copy /Y sg_upgrade.bat  %HOME_DIRECTORY%
+copy /Y upgrade_params.bat  %HOME_DIRECTORY%
 echo 0 > %HOME_DIRECTORY%\current_version
 
 >>data.txt type nul
 
-copy -f baretail.exe %HOME_DIRECTORY%
+copy /Y baretail.exe %HOME_DIRECTORY%
 echo > %HOME_DIRECTORY%\instamsg.log
 
 taskkill /IM monitor.bat /F
@@ -26,11 +26,11 @@ taskkill /IM instamsg.exe /F
 
 copy -f instamsg.lnk "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 
-copy -f %1.exe %HOME_DIRECTORY%
+copy /Y %1.exe %HOME_DIRECTORY%
 move %HOME_DIRECTORY%\%1.exe %HOME_DIRECTORY%\instamsg.exe
-copy -f %1.ipdb %HOME_DIRECTORY%
+copy /Y %1.ipdb %HOME_DIRECTORY%
 move %HOME_DIRECTORY%\%1.ipdb %HOME_DIRECTORY%\instamsg.ipdb
-copy -f %1.iobj %HOME_DIRECTORY%
+copy /Y %1.iobj %HOME_DIRECTORY%
 move %HOME_DIRECTORY%\%1.iobj %HOME_DIRECTORY%\instamsg.iobj
-copy -f %1.pdb %HOME_DIRECTORY%
+copy /Y %1.pdb %HOME_DIRECTORY%
 move %HOME_DIRECTORY%\%1.pdb %HOME_DIRECTORY%\instamsg.pdb
