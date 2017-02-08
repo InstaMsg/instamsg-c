57dd7bd5a63fb4fe9a965a39efcdcd526324057b
Ajay Garg <ajay.garg@sensegrow.com>
2017-02-08 17:48:16 +0530

    don't remove pm-utils

--- setups/lanner-pc/upgrade-from-301-to-302-without-internet.sh
+++ setups/lanner-pc/upgrade-from-301-to-302-without-internet.sh
@@ -4,8 +4,6 @@ set -e
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash intel_idle.max_cstate=0 processor.max_cstate=0 idle=poll"/g' /etc/default/grub
 update-grub
 
-apt-get -y remove pm-utils
-
 kill -9 `pgrep -x instamsg` || true
 cp energy-meter_lanner-pc_3.0.9_3.0.9 /home/sensegrow/instamsg
 
