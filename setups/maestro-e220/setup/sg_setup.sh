66683805dbab6f42bdffd2922ee21e4171ceec53
Ajay Garg <ajay.garg@sensegrow.com>
2017-02-09 09:55:06 +0530

    latest binary (normal, OTA)

--- setups/maestro-e220/setup/sg_setup.sh
+++ setups/maestro-e220/setup/sg_setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 . ../upgrade_params
-PKG_VERSION="4"
+PKG_VERSION="5"
 
 ###################### PERFORM ACTIONS NOW ##################################
 
 /usr/bin/killall monitor.sh || true
 /usr/bin/killall instamsg || true
 
-cp energy-meter_maestro-e220_3.0.9_3.0.9 ${HOME_DIRECTORY}/instamsg
+cp energy-meter_maestro-e220_4.0.0_4.0.0 ${HOME_DIRECTORY}/instamsg
 cp upgrade_params ${HOME_DIRECTORY}
 cp -f sg_upgrade.sh ${HOME_DIRECTORY}
