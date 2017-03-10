e0802a294437777c01242119b9efba828f324011
Ajay Garg <ajay.garg@sensegrow.com>
2017-03-10 15:09:29 +0530

    calibration feature for lanner-pc

--- setups/lanner-pc/upgrade-from-301-to-302-without-internet.sh
+++ setups/lanner-pc/upgrade-from-301-to-302-without-internet.sh
@@ -7,8 +7,21 @@ update-grub
 apt-get -y remove pm-utils
 dpkg -i pm-utils_1.4.1-13ubuntu0.2_all.deb
 
+cp ../../sg_upgrade_try.sh /home/sensegrow
+chmod 777 /home/sensegrow/sg_upgrade_try.sh
+
+cp ../../sg_upgrade.sh /home/sensegrow
+chmod 777 /home/sensegrow/sg_upgrade.sh
+
+cp upgrade_params /home/sensegrow
+chmod 777 /home/sensegrow/upgrade_params
+
+echo 4 > /home/sensegrow/current_version
+
+crontab < cron
+
 kill -9 `pgrep -x instamsg` || true
-cp energy-meter_lanner-pc_4.2.6_4.2.6 /home/sensegrow/instamsg
+cp energy-meter_lanner-pc_4.4.0_4.4.0 /home/sensegrow/instamsg
 
 echo
 echo
