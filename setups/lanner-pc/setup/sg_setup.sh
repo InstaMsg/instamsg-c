353ecd1c3f3628fc9b268bebb6d39005a1ded0e8
Ajay Garg <ajay.garg@sensegrow.com>
2017-03-20 18:24:46 +0530

    updating binary

--- setups/lanner-pc/setup/sg_setup.sh
+++ setups/lanner-pc/setup/sg_setup.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 . ../upgrade_params
-PKG_VERSION="4"
+PKG_VERSION="5"
 
 ###################### PERFORM ACTIONS NOW ##################################
+
+crontab < cron
 
 /usr/bin/killall monitor.sh || true
 /usr/bin/killall instamsg || true
 
-cp energy-meter_lanner-pc_3.0.9_3.0.9 ${HOME_DIRECTORY}/instamsg
+cp energy-meter_lanner-pc_5.3.0_5.3.0 ${HOME_DIRECTORY}/instamsg
 cp upgrade_params ${HOME_DIRECTORY}
 cp -f sg_upgrade.sh ${HOME_DIRECTORY}
