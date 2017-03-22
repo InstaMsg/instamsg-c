9c8078d799937800767c33a304cdf313093366e7
Ajay Garg <ajay.garg@sensegrow.com>
2017-03-22 17:43:15 +0530

    minor

--- setups/lanner-pc/upgrade-from-301-to-302-without-internet.sh
+++ setups/lanner-pc/upgrade-from-301-to-302-without-internet.sh
@@ -4,9 +4,6 @@ set -e
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash intel_idle.max_cstate=0 processor.max_cstate=0 idle=poll"/g' /etc/default/grub
 update-grub
 
-apt-get -y remove pm-utils
-dpkg -i pm-utils_1.4.1-13ubuntu0.2_all.deb
-
 cp ../../sg_upgrade_try.sh /home/sensegrow
 chmod 777 /home/sensegrow/sg_upgrade_try.sh
 
