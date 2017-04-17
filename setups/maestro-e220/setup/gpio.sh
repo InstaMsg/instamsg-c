e3d68fbd05be588d363de85c2a4956bc858bae62
Ajay Garg <ajay.garg@sensegrow.com>
2017-04-17 11:48:34 +0530

    missed a commit

--- setups/maestro-e220/setup/gpio.sh
+++ setups/maestro-e220/setup/gpio.sh
@@ -1,4 +1,4 @@
 /bin/echo 30 > /sys/class/gpio/export
 /bin/echo 31 > /sys/class/gpio/export
 /bin/echo in > /sys/class/gpio/gpio30/direction
-/bin/echo in > /sys/class/gpio/gpio31/direction
+/bin/echo out > /sys/class/gpio/gpio31/direction
