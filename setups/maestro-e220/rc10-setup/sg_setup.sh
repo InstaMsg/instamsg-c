08a20012c129572a5f1bb89218240be3c4ca2680
Ajay Garg <ajay.garg@sensegrow.com>
2017-05-23 00:28:46 +0530

    resilience

--- setups/maestro-e220/rc10-setup/sg_setup.sh
+++ setups/maestro-e220/rc10-setup/sg_setup.sh
@@ -5,6 +5,7 @@ PKG_VERSION="2"
 
 ###################### PERFORM ACTIONS NOW ##################################
 
+/usr/bin/killall monitor.sh || true
 /usr/bin/killall instamsg || true
-cp -f energy-meter_maestro-e220_6.5.0_6.5.0 ${HOME_DIRECTORY}/instamsg
+cp energy-meter_maestro-e220_6.5.0_6.5.0 ${HOME_DIRECTORY}/instamsg
 
